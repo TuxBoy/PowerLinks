ba8cdcac35b8e8ea137a51951f63f643415bbde4
TuxBoy <benoit.mallo@gmail.com>
2020-01-01 11:39:32 +0100

    feat(imp insert method)

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -47,9 +47,11 @@ class Connection
 
 	public function insert(string $tableName, array $data): int
 	{
+		$fieldsName    = join(', ', array_keys($data));
+		$prepareFields = join(', ', array_map(fn () => '?', array_keys($data)));
 		$statement = $this
 			->getConnection()
-			->prepare("INSERT INTO $tableName (url, description) VALUES (?, ?)");
+			->prepare("INSERT INTO $tableName ($fieldsName) VALUES ($prepareFields)");
 
 		$statement->execute(array_values($data));
 
