1fa0f3760095fc4724eca8a308b062cd589ed0ce
TuxBoy <benoit.mallo@gmail.com>
2020-02-03 22:37:10 +0100

    feat(by default, hide form link)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
 import '../css/app.scss'
+
+const showFormAddLink = document.querySelector('#showFormAddLink')
+
+if (showFormAddLink) {
+    showFormAddLink.addEventListener('click', event => {
+        document.querySelector('#addFormLink').classList.toggle('hide')
+    })
+    document.addEventListener('keydown', event => {
+        event.preventDefault()
+        if (event.key === 't') {
+            document.querySelector('#addFormLink').classList.toggle('hide')
+            const urlInput = document.querySelector('#addFormLink #urlInput')
+            urlInput.focus()
+        }
+    })
+}
