ee327be4a0844d39485239546b6f00080ae86aac
TuxBoy <benoit.mallo@gmail.com>
2020-02-04 23:24:38 +0100

    fix(dont preventDefault)

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,18 @@ const showFormAddLink = document.querySelector('#showFormAddLink')
 
 if (showFormAddLink) {
     showFormAddLink.addEventListener('click', event => {
+        event.preventDefault()
         document.querySelector('#addFormLink').classList.toggle('hide')
     })
     document.addEventListener('keydown', event => {
-        if (event.key === 't') {
-            //event.preventDefault()
+        const addFormLink = document.querySelector('#addFormLink')
+        if (event.key === 't' && addFormLink.classList.contains('hide')) {
+            event.preventDefault()
             document.querySelector('#addFormLink').classList.toggle('hide')
             const urlInput = document.querySelector('#addFormLink #urlInput')
             urlInput.focus()
+        } else if (event.key === 'Escape') {
+            addFormLink.classList.toggle('hide')
         }
     })
 }
